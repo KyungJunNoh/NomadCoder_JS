ed0a7fe576f067e21de96f8f996a166adef6dc91
KyungJunNoh <developerjun0615@gmail.com>
2021-01-03 23:42:17 +0900

    이름

--- 실습_Code/index.js
+++ 실습_Code/index.js
@@ -1,19 +1,6 @@
-const nicoInfo = {
-    name : "Nico",
-    age : 33,
-    gender : "Male",
-    isHandsome : true,
-    favMovies : ["Along the gods","LOTR","Oldboy"],
-    favFood : [
-        {
-            name : "Kimchi",
-            fatty : false
-        },
-        {
-            name:"Cheese burger",
-            fatty: true
-        }
-    ]
+function sayHello(){
+    console.log('hello!');
 }
 
-console.log(nicoInfo.favFood[0].fatty);
+sayHello();
+console.log("hi!");
