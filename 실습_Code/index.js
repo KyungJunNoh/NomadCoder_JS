153df7ce8319268b1a7fc73d538113689cb27939
KyungJunNoh <developerjun0615@gmail.com>
2021-01-07 21:15:46 +0900

    NGJ

--- 실습_Code/index.js
+++ 실습_Code/index.js
@@ -1,5 +1,25 @@
-if (20 >5 && "nicolas"==="nicolas"){
-    console.log("Yes")
-} else{
-    console.log("No")
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+
 }
+
+function handleClick(){
+    console.log(title.style.color);
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
